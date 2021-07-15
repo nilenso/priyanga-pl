f7195614a6a756461cf3b0cd317c916e44b75beb
PriyangaPKini <priyangapkini99@gmail.com>
2021-07-15 05:40:04 +0530

    code refactor

--- functions/functions.clj
+++ functions/functions.clj
@@ -1,5 +1,5 @@
-(ns user
-  (:use [clojure.repl]))
+(ns user-2
+  (:require [clojure.repl]))
 
 ;; let bindings
 ;; let - assign a value to a symbol
@@ -14,12 +14,17 @@
 
 ;;functions - verbs
 (inc 1)
-(let [x 1] (+ x 1))
-((fn [x] (+ x 1)) 1) ;; (fn [x] (+ x 1)) equivalent to #(+ % 1)
+(let [x 1] 
+  (+ x 1))
+((fn [x]
+   (+ x 1))
+ 1) ;; (fn [x] (+ x 1)) equivalent to #(+ % 1)
 (#(+ % 1) 1)
-(let [increment #(+ % 1)] (increment 1))
+(let [increment #(+ % 1)] 
+  (increment 1))
 
-(let [twice #(* 2 %)] (+ (twice 1) (twice 3)))
+(let [twice #(* 2 %)] 
+  (+ (twice 1) (twice 3)))
 ;; equivalent to
 (+ (* 2 1) (* 2 3))
 
@@ -35,15 +40,27 @@
 (def numbers [])
 (count numbers)
 (def numbers [2 3]) ;re-defined var 
+(conj numbers 4 5)
+(conj numbers 5)
+(conj numbers 4)
+(conj (conj numbers 4) 5)
+
+
+(prn numbers)
 (count numbers)
 
 
 ;;named functions
-(defn twice ([number] (* 2 number)) ([] (* 2)))
+(defn twice 
+  ([number] 
+   (* 2 number)) 
+  ([] (* 2)))
 (twice 3)
 (twice)
 
-(defn vargs [x y & more-args] {:x x :y y :more more-args})
+(defn vargs 
+  [x y & more-args] 
+  {:x x :y y :more more-args})
 (vargs 1 3)
 (vargs 1 3 5 7 9)
 
@@ -53,9 +70,10 @@
    :static true}
   [x y] 
   (+ x y))
-(add 1 2)
+(add 1 2 )
 (meta #'user/add)
-(doc add)
+(doc add)
+
 
 
 ;;types
