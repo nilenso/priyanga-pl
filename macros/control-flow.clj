ec6bee180d23ba0292a04a5777455bce03255ead
PriyangaPKini <priyangapkini99@gmail.com>
2021-07-21 09:05:45 +0530

    code refactor

--- macros/control-flow.clj
+++ macros/control-flow.clj
@@ -9,16 +9,6 @@
     (println "two"))
   nil)
 
-
-;Returns a list of pos-nums in the arg vector else "no positive numbers"
-(defn positive-number [numbers]
-  (if-let [pos-nums (not-empty (filter pos? numbers))] ;() is not falsey in clojure
-    pos-nums
-    "no positive numbers"))
-
-;test case
-(positive-number [-1 -3 4 -5])
-
 (when true
   (prn :hi)
   (prn :there))
@@ -38,10 +28,12 @@
     2 "n is 2"
     "n is other"))
 
-;test cases
-(case-test 1)
-(case-test 2)
-(case-test 3)
+(defn positive-numbers
+  "Returns a list of pos-nums in the arg vector else 'no positive number' "
+  [numbers]
+  (if-let [pos-nums (not-empty (filter pos? numbers))] ;() is not falsey in clojure
+    pos-nums
+    "no positive number"))
 
 (defn cond-test
   [n]
@@ -50,12 +42,6 @@
     (and (> n 3) (< n 10)) "n is over 3 and under 10"
     :else "n is other"))
 
-;test cases
-(cond-test 1)
-(cond-test 2)
-(cond-test 4)
-
-
 (defn condp-test
   [n]
   (condp contains? n
@@ -63,7 +49,17 @@
     #{4 5 6} "n is either 4 or 5 or 6"
     "n > 6"))
 
-;test cases
+;test case
+(positive-numbers [-1 -3 4 -5])
+
+(case-test 1)
+(case-test 2)
+(case-test 3)
+
+(cond-test 1)
+(cond-test 2)
+(cond-test 4)
+
 (condp-test 3)
 (condp-test 5)
 (condp-test 9)
--- problems/schedule.clj
+++ problems/schedule.clj
@@ -7,18 +7,46 @@
 ;; time.  If a test returns logical true, cond evaluates and returns
 ;; the value of the corresponding expr and doesn't evaluate any of the
 ;; other tests or exprs 
+
 (defn schedule
   [time]
   (cond
     (or (< time 6) (> time 21)) "sleep"
-    (or (= time 6) (and (> time 6) (< time 7))) "morning routine"
-    (or (= time 7) (and (> time 7) (< time 8))) "breakfast"
-    (or (= time 8) (and (> time 8) (< time 13))) "study time"
-    (or (= time 13) (and (> time 13) (< time 14))) "lunch"
-    (or (= time 14) (and (> time 14) (< time 18))) "study time"
-    (or (= time 18) (and (> time 18) (< time 19))) "dinner"
-    (or (= time 19) (and (> time 19) (< time 21))) "fun time"
+    (or (= time 6) (< 6 time 7)) "morning routine"
+    (or (= time 7) (< 7 time 8)) "breakfast"
+    (or (= time 8) (< 8 time 13)) "study time"
+    (or (= time 13) (< 13 time 14))"lunch"
+    (or (= time 14) (< 14 time 18)) "study time"
+    (or (= time 18) (< 18 time 19)) "dinner"
+    (or (= time 19) (< 19 time 21)) "fun time"
     :else "invalid time!"))
+
+(def schedule-1
+  {{:start 21 :end 24} "sleep"
+   {:start 0 :end 6} "sleep"
+   {:start 6 :end 7} "morning routine"
+   {:start 7 :end 8} "breakfast"
+   {:start 8 :end 13} "study time"
+   {:start 13 :end 14} "lunch"
+   {:start 14 :end 18} "study time"
+   {:start 18 :end 19} "dinner"
+   {:start 19 :end 21} "fun time"})
+
+(defn check-schedule?
+  "Returns schedule if start <= time < end else false"
+  [[{:keys [start end]} value] time]
+  (if (or (= time start) (< start time end))
+    value
+    false))
+  
+(defn get-schedule
+  "Returns the schedule at a given time"
+  [time]
+  (loop [kv schedule-1]
+    (let [value (#(check-schedule? % time) (first kv))]
+      (if value
+        value
+        (recur (rest kv))))))
 
 ;; test case
 (schedule 18)
@@ -26,3 +54,6 @@
 (schedule 13)
 (schedule 9)
 (schedule 20)
+
+(get-schedule 2)
+(get-schedule 7)
